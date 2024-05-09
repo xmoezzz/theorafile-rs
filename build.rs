b9ca88eab3c256522936dfacbafe1339fa158d09
xmoe <10909416+xmoezzz@users.noreply.github.com>
2024-05-10 00:28:41 +0800

    fix build script

--- build.rs
+++ build.rs
@@ -1,22 +1,22 @@
 use std::path::Path;
 use walkdir::WalkDir;
 
-fn glob_import<P: AsRef<Path>>(root: P, extenstion: &str, exclude: &str) -> Vec<String> {
+fn glob_import<P: AsRef<Path>>(root: P, extenstion: &str) -> Vec<String> {
     WalkDir::new(root)
         .into_iter()
         .map(|x| x.unwrap())
         .filter(|x| x.path().to_str().unwrap().ends_with(extenstion))
         .map(|x| x.path().to_str().unwrap().to_string())
-        .filter(|x| !x.contains(exclude))
+        // .filter(|x| !x.contains(exclude))
         .collect()
 }
 
 fn main() {
     let mut cc_build = cc::Build::new();
     let mut cfiles = vec![];
-    cfiles.extend(glob_import("upstream/lib/ogg", ".c", ""));
-    cfiles.extend(glob_import("upstream/lib/theora", ".c", ""));
-    cfiles.extend(glob_import("upstream/lib/vorbis", ".c", ""));
+    cfiles.extend(glob_import("upstream/lib/ogg", ".c"));
+    cfiles.extend(glob_import("upstream/lib/theora", ".c"));
+    cfiles.extend(glob_import("upstream/lib/vorbis", ".c"));
     cfiles.push("upstream/theorafile.c".to_string());
     
     cc_build
